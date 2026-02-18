cdefd0a7b68f50eb7ca58fd7d5c7c918df3f4847
Tomáš Greguš <gregustomas@seznam.cz>
2026-02-18 18:38:03 +0100

    refactor: changed backBtn styling

--- heavy-in/components/BackBtn.tsx
+++ heavy-in/components/BackBtn.tsx
@@ -1,27 +1,22 @@
-import { ArrowLeft } from "lucide-react"
+import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 
 interface BackBtnProps {
-    link: string;
+  link: string;
 }
 
-function BackBtn({link}: BackBtnProps) {
+function BackBtn({ link }: BackBtnProps) {
   return (
-    <div className="absolute top-6 left-6 z-50">
-            <Link
-              href={link || "/"}
-              className="flex items-center gap-2 bg-heavy-surface/40 backdrop-blur-xl px-4 py-2 rounded-full border border-heavy-border hover:bg-heavy-surface/60 transition-all shadow-2xl group"
-            >
-              <ArrowLeft
-                size={18}
-                className="text-heavy-teal group-hover:-translate-x-1 transition-transform"
-              />
-              <span className="text-[10px] font-black uppercase tracking-widest text-heavy-main">
-                Back
-              </span>
-            </Link>
-          </div>
-  )
+    <Link
+      href={link || "/"}
+      className="flex items-center justify-center w-10 h-10 bg-heavy-surface/40 backdrop-blur-xl rounded-xl border border-heavy-border hover:bg-heavy-surface/60 transition-all shadow-xl group active:scale-95"
+    >
+      <ArrowLeft
+        size={18}
+        className="text-heavy-teal group-hover:-translate-x-1 transition-transform"
+      />
+    </Link>
+  );
 }
 
-export default BackBtn
+export default BackBtn;
